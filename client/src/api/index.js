2b3b527bb08b32b4402a1080d7be0e8bb0d54054
Ivan <ivandonyk@gmail.com>
2022-07-08 04:35:32 +0100

    chore: migrate api logic from client to server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,67 +1,16 @@
-import Papa from "papaparse";
-import csv from "../data/data.csv";
+import axios from "axios";
 
-export const getJSONFromCSV = async () => {
-  let json;
-
-  const csvBlob = await fetch(csv);
-  const csvString = await csvBlob.text();
-
-  Papa.parse(csvString, {
-    header: true,
-    skipEmptyLines: true,
-    complete: (results) => {
-      json = results.data;
-    },
+export const fetchMembers = async () =>
+  axios.get("/members").then(({ data }) => {
+    return data;
   });
 
-  return json;
-};
-
-export const fetchMembers = async () => {
-  const jsonData = await getJSONFromCSV();
-
-  const membersArray = jsonData.map(({ member }) => member);
-  const members = [...new Set(membersArray)];
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(members);
-    }, 500);
-  });
-};
-
-export const fetchGames = async () => {
-  const members = await fetchMembers();
-  const jsonData = await getJSONFromCSV();
-
-  const games = members.map((member) => {
-    const gamesCount = jsonData.filter((data) => data.member === member).length;
-
-    return { member, count: gamesCount };
+export const fetchGames = async () =>
+  axios.get("/games").then(({ data }) => {
+    return data;
   });
 
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(games);
-    }, 500);
+export const fetchMemberGames = async (memberName) =>
+  axios.get(`/member_games/${memberName}`).then(({ data }) => {
+    return data;
   });
-};
-
-export const fetchMemberGames = async (memberName) => {
-  const jsonData = await getJSONFromCSV();
-
-  const gamesCounts = ["Chess", "Tennis", "Soccer"].map((game) => {
-    const gameCount = jsonData.filter(
-      (data) => data.member === memberName && data.game === game
-    ).length;
-
-    return [game, gameCount];
-  });
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(gamesCounts);
-    }, 200);
-  });
-};
--- server/games.data.js
+++ server/games.data.js
@@ -5,7 +5,6 @@ let jsonData = [];
 export const loadDataFromCSV = async () => {
   try {
     const json = await csv().fromFile("./data/data.csv");
-
     jsonData = json;
   } catch (error) {
     throw error;
