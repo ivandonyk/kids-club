808a1853329e044abda078b0319476bf97630a57
Ivan <ivandonyk@gmail.com>
2022-07-08 04:05:30 +0100

    feat(server): convert csv to json array

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import CreateServer from "./create-server.js";
+import { loadDataFromCSV } from "./games.data.js";
 
-const PORT = 3050;
+const PORT = 4000;
 
 const ServerInstance = CreateServer();
 
 ServerInstance.listen(PORT, async () => {
+  await loadDataFromCSV();
   console.log(`Local API listening at http://localhost:${PORT}`);
 });
